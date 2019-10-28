28814ab6b6a80893b610cf1e046c1122e1122f1c
Bogdan <balajbogdan89@icloud.com>
2019-10-28 21:20:56 +0200

    lab1

---
+++ src/Challenge1/Main.java
@@ -0,0 +1,25 @@
+package Challenge1;
+
+public class Main {
+    void fizzBuzz(){
+        int i = 1;
+        for(i =1;i <= 100;i++) {
+            if(i % 3 == 0) {
+                System.out.println("Fizz");
+            }else{
+                if(i % 5 == 0) {
+                    System.out.println("Buzz");
+                }else{
+                    System.out.println(i);
+                }
+            }
+            System.out.println(i);
+        }
+    }
+    public static void main (String args []) {
+        System.out.println("Atelierul Digital 2019");
+        Main a = new Main ();
+        a.fizzBuzz();
+
+    }
+}
